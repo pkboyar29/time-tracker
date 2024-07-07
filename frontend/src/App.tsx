439459a4e9ee840cc81544dafa7d2102152ed504
Eduard Musatov <pkboyar09@gmail.com>
2024-07-07 11:13:05 +0300

    implement simple count down timer with ability to enter seconds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+interface SessionFields {
+  spentTimeSeconds: number
+}
+
 function App() {
+  const [started, setStarted] = useState<boolean>(false)
+  const [enabled, setEnabled] = useState<boolean>(false)
+  const [countDown, setCountDown] = useState<number>(0)
+
+  const { register, getValues } = useForm<SessionFields>({ mode: 'onBlur' })
+
+  const toggleTimer = () => {
+    setEnabled((e) => !e)
+  }
+
+  const startTimer = () => {
+    setCountDown(getValues('spentTimeSeconds'))
+    setStarted(true)
+    toggleTimer()
+  }
+
+  const stopTimer = () => {
+    setStarted(false)
+    setEnabled(false)
+    setCountDown(getValues('spentTimeSeconds'))
+  }
+
+  useEffect(() => {
+    if (!enabled)
+      return
+    const i = setInterval(() => {
+      setCountDown((c) => Math.max(c - 1, 0))
+    }, 1000)
+    return () => {
+      clearInterval(i)
+    }
+  }, [enabled])
+
+  useEffect(() => {
+    if (countDown === 0) {
+      alert('Count down')
+      stopTimer()
+    }
+  }, [countDown])
 
   return (
     <>
-      App
+      <div>
+        <input {...register('spentTimeSeconds')} type='number' placeholder='Enter seconds' className='p-1 rounded-md' />
+        <div className=''>{countDown}</div>
+        <div className='mt-3 flex justify-center gap-4'>
+          <button onClick={() => {
+            if (!started)
+              startTimer()
+            else
+              toggleTimer()
+          }}>{!started ? 'Start' : (enabled ? 'Stop' : 'Continue')}</button>
+          {(started) && (<button onClick={stopTimer}>Reset</button>)}
+        </div>
+      </div>
     </>
   )
 }
