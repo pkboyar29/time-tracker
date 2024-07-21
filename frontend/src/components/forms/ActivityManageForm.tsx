b43b087c3e8207d7d5272effb0b484e7ebe8a979
Eduard Musatov <pkboyar09@gmail.com>
2024-07-21 11:24:05 +0300

    create separate activity manage form

--- frontend/src/components/forms/ActivityManageForm.tsx
+++ frontend/src/components/forms/ActivityManageForm.tsx
@@ -1,8 +1,51 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '../../redux/store'
+import { createActivity, updateActivity } from '../../redux/slices/activitySlice'
 
-const ActivityManageForm: FC = () => {
+interface ActivityManageFormProps {
+   currentActivity: IActivity | null,
+   afterSubmitHandler: () => void
+}
+
+interface ActivityFields {
+   name: string,
+   descr?: string
+}
+
+const ActivityManageForm: FC<ActivityManageFormProps> = ({ currentActivity, afterSubmitHandler }) => {
+   const dispatch = useDispatch<AppDispatch>()
+
+   const { register, handleSubmit } = useForm<ActivityFields>({
+      mode: 'onBlur',
+      defaultValues: {
+         name: currentActivity?.name,
+         descr: currentActivity?.descr
+      }
+   })
+
+   const onSubmit = (data: ActivityFields) => {
+      if (!currentActivity) {
+         dispatch(createActivity(data))
+      } else {
+         dispatch(updateActivity({
+            id: currentActivity.id,
+            ...data
+         }))
+      }
+      afterSubmitHandler()
+   }
+
    return (
-      <></>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-start gap-3'>
+         <input {...register('name')} type='text' placeholder='Enter name' className='w-full p-1 rounded-md bg-red-500 text-white placeholder-white' />
+         <textarea {...register('descr')} placeholder='Enter description (optional)' className='w-full h-20 p-1 rounded-md bg-red-500 text-white placeholder-white' />
+         <div className='flex gap-4'>
+            <button type='submit' className='p-3 bg-red-500 text-white rounded-xl'>{!currentActivity ? 'Create activity' : 'Update activity'}</button>
+         </div>
+      </form>
+
    )
 }
 
--- frontend/src/pages/ActivitiesPage.tsx
+++ frontend/src/pages/ActivitiesPage.tsx
@@ -5,15 +5,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { RootState, AppDispatch } from '../redux/store'
 import { fetchActivities, createActivity, updateActivity, deleteActivity } from '../redux/slices/activitySlice'
 
+import ActivityManageForm from '../components/forms/ActivityManageForm'
 import SessionCreateForm from '../components/forms/SessionCreateForm'
 import ActivityItem from '../components/ActivityItem'
 import Button from '../components/Button'
 import Modal from '../components/Modal'
-
-interface ActivityFields {
-   name: string,
-   descr?: string
-}
 
 const ActivitiesPage: FC = () => {
    const activities = useSelector((state: RootState) => state.activities.activities)
@@ -25,17 +21,12 @@ const ActivitiesPage: FC = () => {
    const dispatch = useDispatch<AppDispatch>()
    const navigate = useNavigate()
 
-   const { register, handleSubmit, setValue, reset } = useForm<ActivityFields>({ mode: 'onBlur' })
-
    useEffect(() => {
       dispatch(fetchActivities())
    }, [])
 
    const onEditActivityClick = (activity: IActivity) => {
       setCurrentActivity(activity)
-      setValue('name', activity.name)
-      setValue('descr', activity.descr)
-
       setManageModal(true)
    }
 
@@ -44,35 +35,17 @@ const ActivitiesPage: FC = () => {
       dispatch(deleteActivity(activityId))
    }
 
-   const refreshForm = () => {
-      setCurrentActivity(null)
-      reset()
-      setManageModal(false)
-   }
-
-   const onSubmit = (data: ActivityFields) => {
-      if (!currentActivity) {
-         dispatch(createActivity(data))
-      } else {
-         dispatch(updateActivity({
-            id: currentActivity.id,
-            ...data
-         }))
-      }
-      refreshForm()
-   }
-
    return (
       <>
          {manageModal
-            && <Modal title={!currentActivity ? 'Creating a new activity' : 'Updating an activity'} onCloseModal={() => setManageModal(false)}>
-               <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-start gap-3'>
-                  <input {...register('name')} type='text' placeholder='Enter name' className='w-full p-1 rounded-md bg-red-500 text-white placeholder-white' />
-                  <textarea {...register('descr')} placeholder='Enter description (optional)' className='w-full h-20 p-1 rounded-md bg-red-500 text-white placeholder-white' />
-                  <div className='flex gap-4'>
-                     <button type='submit' className='p-3 bg-red-500 text-white rounded-xl'>{!currentActivity ? 'Create activity' : 'Update activity'}</button>
-                  </div>
-               </form>
+            && <Modal title={!currentActivity ? 'Creating a new activity' : 'Updating an activity'} onCloseModal={() => {
+               setManageModal(false)
+               setCurrentActivity(null)
+            }}>
+               <ActivityManageForm currentActivity={currentActivity} afterSubmitHandler={() => {
+                  setCurrentActivity(null)
+                  setManageModal(false)
+               }} />
             </Modal>}
 
          {deleteModal
