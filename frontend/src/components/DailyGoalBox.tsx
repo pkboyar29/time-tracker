9f4c3f5812ba9a5ebce53240018679da2a2d63d5
Eduard Musatov <pkboyar09@gmail.com>
2025-04-06 10:46:15 +0300

    daily goal box fixes

--- frontend/src/components/DailyGoalBox.tsx
+++ frontend/src/components/DailyGoalBox.tsx
@@ -11,23 +11,25 @@ const DailyGoalBox: FC<DailyGoalBoxProps> = ({
   dailyGoalSeconds,
 }) => {
   const dailyGoalPercent =
-    (Math.trunc(spentTimeSeconds / 60) / Math.trunc(dailyGoalSeconds / 60)) *
-    100;
+    spentTimeSeconds > dailyGoalSeconds
+      ? 100
+      : (Math.trunc(spentTimeSeconds / 60) /
+          Math.trunc(dailyGoalSeconds / 60)) *
+        100;
 
   return (
     <div className="px-10 py-5 bg-white rounded-lg">
-      <div className="flex gap-20">
-        <div className="w-[140px]">
-          <CustomCircularProgress
-            valuePercent={dailyGoalPercent}
-            label={`Daily goal: ${dailyGoalSeconds / 60}m`}
-            size="verybig"
-          />
-        </div>
+      <div className="flex items-center gap-10">
+        <CustomCircularProgress
+          valuePercent={dailyGoalPercent}
+          label={`Daily goal: ${dailyGoalSeconds / 60}m`}
+          size="verybig"
+        />
         <div className="text-xl">
           You acheived{' '}
           <span className="font-bold">
-            {Math.trunc(spentTimeSeconds / 60)} minutes
+            {Math.trunc(spentTimeSeconds / 60)} minutes /{' '}
+            {Math.trunc(dailyGoalSeconds / 60)} minutes
           </span>
         </div>
       </div>
