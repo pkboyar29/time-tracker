41f7472ea207e21ec65e55c590b0919b845a3f34
Eduard Musatov <pkboyar09@gmail.com>
2025-09-29 21:02:23 +0300

    temporarily stopped using DailyRotateFile

--- backend/logger.ts
+++ backend/logger.ts
@@ -1,17 +1,20 @@
 import winston from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file';
+// import DailyRotateFile from 'winston-daily-rotate-file';
 
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new DailyRotateFile({
-      filename: 'logs/%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: false,
-      maxSize: '20m',
-      maxFiles: '14d', // store 14 days
+    //   new DailyRotateFile({
+    // filename: 'logs/%DATE%.log',
+    // datePattern: 'YYYY-MM-DD',
+    // zippedArchive: false,
+    // maxSize: '20m',
+    // maxFiles: '14d', // store 14 days
+    // }),
+    new winston.transports.File({
+      filename: new Date().toISOString().split('T')[0] + '.log',
     }),
   ],
 });
